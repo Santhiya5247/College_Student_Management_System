2a3a46d3f907e6147d5304aeffdaa68521a43831
Santhiya5247 <146969845+Santhiya5247@users.noreply.github.com>
2026-07-26 01:40:51 -0700

    Add frontend files

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "../styles/Header.css";
+function Header() {
+  return (
+    <nav className="navbar navbar-dark bg-primary shadow">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">
+          🎓 College Student Management System
+        </span>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
